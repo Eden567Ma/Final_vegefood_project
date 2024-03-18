2afa4323a4af0d874ecb0226afec77e90ef638db
Massilva_OUAKED <98214535+Eden567Ma@users.noreply.github.com>
2024-03-18 14:21:53 +0100

    Update calendar.js

--- public/backend/js/calendar.js
+++ public/backend/js/calendar.js
@@ -8,28 +8,28 @@
           center: 'title',
           right: 'month,basicWeek,basicDay'
         },
-        defaultDate: '2017-07-12',
-        navLinks: true, // can click day/week names to navigate views
+        defaultDate: '2022-05-12',
+        navLinks: true, 
         editable: true,
-        eventLimit: true, // allow "more" link when too many events
+        eventLimit: true, 
         events: [{
             title: 'All Day Event',
-            start: '2017-07-08'
+            start: '2022-05-12'
           },
           {
             title: 'Long Event',
-            start: '2017-07-01',
-            end: '2017-07-07'
+            start: '2022-05-12',
+            end: '2022-05-12'
           },
           {
             id: 999,
             title: 'Repeating Event',
-            start: '2017-07-09T16:00:00'
+            start: ''2022-05-12 T16:00:00'
           },
           {
             id: 999,
             title: 'Repeating Event',
-            start: '2017-07-16T16:00:00'
+            start: ''2022-05-12T16:00:00'
           },
           {
             title: 'Conference',
@@ -55,16 +55,16 @@
           },
           {
             title: 'Dinner',
-            start: '2017-07-12T20:00:00'
+            start: '2022-05-12'
           },
           {
             title: 'Birthday Party',
-            start: '2017-07-13T07:00:00'
+            start: '2022-05-12'
           },
           {
             title: 'Click for Google',
             url: 'http://google.com/',
-            start: '2017-07-28'
+            start: '2022-05-12'
           }
         ]
       })
